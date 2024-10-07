4dc8ecd8d41e602783248d80d75b9c9e4890c6ca
Lorenzo <lorenzobefera0@gmail.com>
2024-10-07 18:58:13 +0200

    Refactor count_lines.py to separate logic into three functions: count_lines_in_file, list_files, and print_total_lines, improving code organization and readability.

--- count_lines.py
+++ count_lines.py
@@ -1,22 +1,45 @@
 import os
 import sys
 
-def count_lines(path, extensions):
-  os.chdir(path)
-  lines = 0
+def count_lines_in_file(file):
+  with open(file, 'r') as f:
+    file_lines = sum(1 for line in f)
+  return file_lines
+
+def list_files(path, extensions):
+  if not os.path.isdir(path):
+    return []
+  else:
+    path = os.path.abspath(path)
+  files = []
   
-  for file in os.listdir():
+  for file in os.listdir(path):
+    if os.path.isdir(file):
+      files.extend(list_files(os.path.abspath(file), extensions))
+            
     if any(file.endswith(ext) for ext in extensions):
-      with open(file, 'r') as f:
-        file_lines = sum(1 for line in f)
-      lines += file_lines
-      print(f"file: {file} +{file_lines}")
-  print(f'You wrote a total of {lines} lines of code')
+      files.append(os.path.abspath(os.path.join(path, file)))
+      
+  return files
+
+def print_total_lines(path, extensions):
+  path = os.path.abspath(path)
+  os.chdir(path)
+  files = list_files(path, extensions)
+  
+  total_lines = 0
+  
+  for file in files:
+    file_lines = count_lines_in_file(file)
+    total_lines += file_lines
+    
+    print(f"file: {file} +{file_lines}")
+  print(f'You wrote a total of {total_lines} lines of code')
 
 if __name__ == '__main__':
   if len(sys.argv) < 3:
-    print("Usage: python3 count_lines.py <path> <extensions...>")
-    print("Example: python3 count_lines.py /home/Desktop .c .cpp .hpp")
+    print(f"Usage: python3 {sys.argv[0]} <path> <extensions...>")
+    print(f"Example: python3 {sys.argv[0]} /home/Project .cpp .h .hpp")
     sys.exit(1)
     
-  count_lines(sys.argv[1], sys.argv[2:])
+  print_total_lines(sys.argv[1], sys.argv[2:])
